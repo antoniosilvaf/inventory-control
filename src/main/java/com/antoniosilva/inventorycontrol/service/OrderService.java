f62c61271d12697ebe7f86de0a6251ebd032c1a0
Antonio Silva <antoniosergio21@hotmail.com>
2022-12-15 18:09:19 -0300

    feat: Order endpoints

--- src/main/java/com/antoniosilva/inventorycontrol/service/OrderService.java
+++ src/main/java/com/antoniosilva/inventorycontrol/service/OrderService.java
@@ -1,12 +1,17 @@
 package com.antoniosilva.inventorycontrol.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.antoniosilva.inventorycontrol.model.Order;
 import com.antoniosilva.inventorycontrol.repository.OrderRepository;
+import com.antoniosilva.inventorycontrol.service.exceptions.DatabaseException;
+import com.antoniosilva.inventorycontrol.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class OrderService {
@@ -17,5 +22,24 @@ public class OrderService {
     public List<Order> findAll() {
         return repository.findAll();
     }
+
+    public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Order insert(Order obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
 
 }

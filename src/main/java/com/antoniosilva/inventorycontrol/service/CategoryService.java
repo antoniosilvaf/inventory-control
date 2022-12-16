00b6dfe0a6b3e3b2ac84630ea51e2a1040c08dc5
Antonio Silva <antoniosergio21@hotmail.com>
2022-12-16 15:23:10 -0300

    feat: Category endpoints

--- src/main/java/com/antoniosilva/inventorycontrol/service/CategoryService.java
+++ src/main/java/com/antoniosilva/inventorycontrol/service/CategoryService.java
@@ -1,12 +1,19 @@
 package com.antoniosilva.inventorycontrol.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import com.antoniosilva.inventorycontrol.repository.CategoryRepository;
+import com.antoniosilva.inventorycontrol.service.exceptions.DatabaseException;
+import com.antoniosilva.inventorycontrol.service.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 import com.antoniosilva.inventorycontrol.model.Category;
-import com.antoniosilva.inventorycontrol.repository.CategoryRepository;
 
 @Service
 public class CategoryService {
@@ -17,4 +24,37 @@ public class CategoryService {
     public List<Category> findAll() {
         return repository.findAll();
     }
+
+    public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Category update(Long id, Category obj) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());;
+	}
 }
